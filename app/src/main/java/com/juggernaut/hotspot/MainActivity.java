faf0f1af6757253522b87f1ba51be11790088e00
sofiane <sofiane@skylights.aero>
2017-08-21 14:28:30 +0200

    ajout du test lors de l'appui sur l'image (onCLick)

--- app/src/main/java/com/juggernaut/hotspot/MainActivity.java
+++ app/src/main/java/com/juggernaut/hotspot/MainActivity.java
@@ -2,31 +2,40 @@ package com.juggernaut.hotspot;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.provider.Settings;
 import android.support.design.widget.TextInputLayout;
+import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Spinner;
 import android.widget.TextView;
 
-import static com.juggernaut.hotspot.OnOfHotspot.spin;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
 
-public class MainActivity extends Activity implements View.OnClickListener {
 
-    public EditText hotspotNameText;
+public class MainActivity extends Activity implements View.OnClickListener
+{
+
     public Button enableHotspot;
-    public EditText passwordText;
     public TextView status;
     public Boolean aBoolean;
-    public Spinner spinner;
     public TextInputLayout textInputLayout;
+    public List<String> IP_Client = new ArrayList<>();
 
+    private Handler myHandler = new Handler();
+    private int delay = 1000;
+
+    //    private Runnable myRunnable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,30 +45,20 @@ public class MainActivity extends Activity implements View.OnClickListener {
         enableHotspot = (Button) findViewById(R.id.enableHotspot);
         enableHotspot.setOnClickListener(this);
 
-        hotspotNameText = (EditText) findViewById(R.id.hotspotName);
-        passwordText = (EditText) findViewById(R.id.passwordId);
-
-        status = (TextView) findViewById(R.id.status);
+        // status = (TextView) findViewById(R.id.status);
 
         aBoolean = OnOfHotspot.isApOn(this);
 
         if (aBoolean) {
             enableHotspot.setText("DISABLE");
-            status.setText("WiFi Hotspot is ON!");
+            //status.setText("WiFi Hotspot is ON!");
         } else {
             enableHotspot.setText("ENABLE");
-            status.setText("WiFi Hotspot is OFF!");
+            //status.setText("WiFi Hotspot is OFF!");
         }
 
-        textInputLayout = (TextInputLayout) findViewById(R.id.jugger);
+        //textInputLayout = (TextInputLayout) findViewById(R.id.jugger);
 
-        spinner = (Spinner) findViewById(R.id.spinner);
-        String[] encryption = {"Open", "WPA-PSK"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, encryption);
-        spinner.setAdapter(adapter);
-
-        passwordText.setText("12345678");
-        hotspotNameText.setText("Wi-Fi Hotspot");
         writePermission();
 
     }
@@ -75,32 +74,59 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
     @Override
     protected void onStart() {
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                if ("WPA-PSK".equals(spinner.getSelectedItem().toString().toUpperCase())) {
-                    spin = 1;
-                    textInputLayout.setVisibility(View.VISIBLE);
-                    passwordText.setVisibility(View.VISIBLE);
+        // Will check if there is a client every seconds
 
-                } else if ("OPEN".equals(spinner.getSelectedItem().toString().toUpperCase())) {
-                    spin = 0;
-                    textInputLayout.setVisibility(View.INVISIBLE);
-                    passwordText.setVisibility(View.INVISIBLE);
+        myHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Button buttonHubSN = (Button) findViewById(R.id.buttonHubSerialNumber);
+                Button buttonAssociateCtsHub = (Button) findViewById(R.id.buttonHubCtsConfig);
+
+                EditText CTS_Number_Text = (EditText) findViewById(R.id.CTS_Number);
+                String CTS_Number = CTS_Number_Text.getText().toString();
+
+                EditText SN_Hub_Text = (EditText) findViewById(R.id.SN_Hub);
+                String SN_Hub = SN_Hub_Text.getText().toString();
+
+                TextView numberOfDevicesConnected = (TextView) findViewById(R.id.devicesConnected);
+
+                if (OnOfHotspot.isApOn(getApplicationContext())) {
+                    // check if Hotspot is enabled before checking for IP clients
+                    getClientList();
+                    if (IP_Client.size() == 1) {
+                        numberOfDevicesConnected.setText("Number of devices connected : 1");
+                        
+                        if (SN_Hub.length() >= 1){
+                            buttonHubSN.setEnabled(true);
+                        }
+                        else{
+                            buttonHubSN.setEnabled(false);
+                        }
+
+                        if (CTS_Number.length() >= 1){
+                            buttonAssociateCtsHub.setEnabled(true);
+                        }
+                        else{
+                            buttonAssociateCtsHub.setEnabled(false);
+                        }
+                    } else if (IP_Client.size() >= 2){
+                        numberOfDevicesConnected.setText("TOO MANY DEVICES CONNECTED");
+                        numberOfDevicesConnected.setTextColor(Color.RED);
+                    }
+                } else {
+                    buttonHubSN.setEnabled(false);
+                    buttonAssociateCtsHub.setEnabled(false);
+                    numberOfDevicesConnected.setText("Number of devices connected : 0");
                 }
+                myHandler.postDelayed(this, delay);
             }
+        }, delay);
 
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-            }
-        });
         super.onStart();
     }
 
     @Override
     public void onClick(View v) {
-
-
         Button toggle = (Button) v;
         String buttonText = toggle.getText().toString();
         if (buttonText.equals("DISABLE")) {
@@ -117,31 +143,79 @@ public class MainActivity extends Activity implements View.OnClickListener {
                 e.printStackTrace();
             }
             toggle.setText("ENABLE");
-            status.setText("WiFi Hotspot is OFF!");
 
         } else if (buttonText.equals("ENABLE")) {
             try {
-                if (!((hotspotNameText.getText().toString()).equals(""))) {
-                    String password = passwordText.getText().toString();
-                    if (!(password.isEmpty() || password.length() < 8 || password.length() > 15)) {
+                OnOfHotspot.hotspotName = "skylightshub";  // name of the default hotspot
+                OnOfHotspot.password = "skylights12";  //password of the default hotspot
+                OnOfHotspot.getApConfiguration(this);
+                OnOfHotspot.configApState(this, true);
 
-                        OnOfHotspot.hotspotName = hotspotNameText.getText().toString();
-                        OnOfHotspot.password = passwordText.getText().toString();
-                        OnOfHotspot.getApConfiguration(this);
-                        OnOfHotspot.configApState(this, true);
-
-                        toggle.setText("DISABLE");
-                        status.setText("WiFi Hotspot is ON!");
-                    } else {
-                        passwordText.setError("between 8 and 15 characters");
-                    }
-                } else {
-                    hotspotNameText.setError("Please Specify Hotspot name!");
-                }
-
+                toggle.setText("DISABLE");
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
     }
+
+    public void setHubSerialNumber(View v)
+    {
+        String SN_Value;
+
+        TextView SN_Hub_Text = (TextView) findViewById(R.id.SN_Hub);
+        SN_Value = SN_Hub_Text.getText().toString();
+
+        WebView myWebview = (WebView) findViewById(R.id.myWebview);
+        //myWebview.setWebViewClient(new WebViewClient());
+        if (SN_Value.length() == 1) {
+            myWebview.loadUrl("http://" + IP_Client.get(0) + "/SETSN?sn=SKY-HUB-TC-000" + SN_Value);
+        } else if (SN_Value.length() == 2) {
+            myWebview.loadUrl("http://" + IP_Client.get(0) + "/SETSN?sn=SKY-HUB-TC-00" + SN_Value);
+        }
+    }
+
+    public void setAssociationHubAndCts(View v)
+    {
+        String CTS_Number;
+
+        TextView CTS_Number_Text = (TextView) findViewById(R.id.CTS_Number);
+        CTS_Number = CTS_Number_Text.getText().toString();
+
+        WebView myWebview = (WebView) findViewById(R.id.myWebview);
+        // myWebview.setWebViewClient(new WebViewClient());
+        myWebview.loadUrl("http://" + IP_Client.get(0) + "/CONFIG?ssid=skylightscts" + CTS_Number + "&pwd=" +
+                "XFncc7wvyXzuME7x2QbeMCbyA4rXt7R7ySmzQDdNy3cswqD7y3h2kPMXp8YN4vz");
+    }
+
+
+    public void getClientList()
+    {
+    //  int ipCount = 0;
+        BufferedReader br;
+        try {
+            br = new BufferedReader(new FileReader("/proc/net/arp"));
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] splitted = line.split(" +");
+                if (splitted != null) {
+                    // Basic sanity check
+                    String ip = splitted[0];
+
+                    // check if ip contains numbers (yes, this char sequence is ununderstable...)
+                    if (ip.matches(".*\\d.*") && !IP_Client.contains(ip)) {
+                        IP_Client.add(ip);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            Log.e("Error read file", " Error reading /proc/net/arp");
+        }
+    }
+
+    public void test(View v)
+    {
+        Log.i("TTTEST", "ONCLICKEEEUH");
+    }
+
+//================================================================================================//
 }
--- app/src/main/java/com/juggernaut/hotspot/OnOfHotspot.java
+++ app/src/main/java/com/juggernaut/hotspot/OnOfHotspot.java
@@ -9,20 +9,16 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 
-/**
- * Created by Kailash on 7/8/2017.
- */
-
 public class OnOfHotspot {
 
     public static WifiConfiguration config;
     public static String hotspotName;
     public static String password;
-    public static int spin;
     //check whether wifi hotspot on or off
 
     public static boolean isApOn(Context context) {
         WifiManager wifimanager = (WifiManager) context.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+
         try {
             Method method = wifimanager.getClass().getDeclaredMethod("isWifiApEnabled");
             method.setAccessible(true);
@@ -33,10 +29,12 @@ public class OnOfHotspot {
     }
 
     // toggle wifi hotspot on or off
-    public static boolean configApState(Context context, boolean apState) {
+    public static boolean configApState(Context context, boolean apState)
+    {
 
         WifiManager wifimanager = (WifiManager) context.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
         wifimanager.setWifiEnabled(false);
+
         try {
             if (apState) {
                 WifiConfiguration netConfig = new WifiConfiguration();
@@ -45,35 +43,21 @@ public class OnOfHotspot {
                 netConfig.status = WifiConfiguration.Status.DISABLED;
                 netConfig.priority = 40;
 
-                if (spin == 0) {
-                    netConfig.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.NONE);
-                    netConfig.allowedProtocols.set(WifiConfiguration.Protocol.RSN);
-                    netConfig.allowedProtocols.set(WifiConfiguration.Protocol.WPA);
-                    netConfig.allowedAuthAlgorithms.clear();
-                    netConfig.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.CCMP);
-                    netConfig.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.TKIP);
-                    netConfig.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.WEP40);
-                    netConfig.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.WEP104);
-                    netConfig.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.CCMP);
-                    netConfig.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.TKIP);
-                } else if (spin == 1) {
+                // spin = 1 -> WPA_PSK protection
+                netConfig.allowedProtocols.set(WifiConfiguration.Protocol.RSN);
+                netConfig.allowedProtocols.set(WifiConfiguration.Protocol.WPA);
+                netConfig.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.WPA_PSK);
+                netConfig.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.CCMP);
+                netConfig.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.TKIP);
+                netConfig.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.WEP40);
+                netConfig.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.WEP104);
+                netConfig.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.CCMP);
+                netConfig.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.TKIP);
 
-                    netConfig.allowedProtocols.set(WifiConfiguration.Protocol.RSN);
-                    netConfig.allowedProtocols.set(WifiConfiguration.Protocol.WPA);
-                    netConfig.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.WPA_PSK);
-                    netConfig.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.CCMP);
-                    netConfig.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.TKIP);
-                    netConfig.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.WEP40);
-                    netConfig.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.WEP104);
-                    netConfig.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.CCMP);
-                    netConfig.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.TKIP);
-
-                    netConfig.preSharedKey = password;
-                }
+                netConfig.preSharedKey = password;
 
                 Method method = wifimanager.getClass().getMethod("setWifiApEnabled", WifiConfiguration.class, boolean.class);
                 method.invoke(wifimanager, netConfig, apState);
-
                 Toast.makeText(context, "WiFi Hotspot is Created!", Toast.LENGTH_SHORT).show();
                 return true;
 
